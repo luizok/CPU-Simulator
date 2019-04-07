d5fc886044ef4d7bc9adfd1ddf71cd7b559b245d
Luiz Felipe <luizfelipesmaciel@gmail.com>
2019-04-07 06:27:37 -0300

    Added WORD macro

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 int main(int argc, char *argv) {
 
-    cpu_word_t word = new_cpu_word(0b001111110000001111001101010101101011);
+    cpu_word_t word = WORD(011, 001010101, 11110010, 001111110, 110, 0101);
 
     printf("TAMAIN = %d\n", (int) sizeof(word));
     print_cpu_word(word);
--- types.h
+++ types.h
@@ -4,5 +4,6 @@
 #define uint16_t unsigned short int
 #define uint32_t unsigned int
 #define byte     unsigned char
+#define WORD(J, A, ALU, C, M, B) new_cpu_word(0b##J##A##ALU##C##M##B)
 
 #endif
