9203c01b2b81e34879dd19936076921d9240b33d
louis ian <louisiangt@gmail.com>
2019-05-02 05:17:27 -0300

    file IO completely working

--- file io test.c
+++ file io test.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+typedef unsigned long microcode;
 
 int main(int argc, char const *argv[])
 {
-    
-    FILE *file;
-    file = fopen("microprog.rom","rb+");
-    
-    unsigned long teste[512];
-    
-    //fwrite (address_data,size_data,numbers_data,pointer_to_file);
+    microcode teste[512];
     for(int i = 0; i < 512; i++)
-    {
-        teste[i] = fread( (unsigned long*)&(teste), 512*sizeof(unsigned long), 512, file);
-        //teste[i] = 0b0;
-    }
+    teste[i] = 0b0000000000000000000000000000000000000000000000000000000000000000;
 
-    //teste[511] = 0b0000000000000000000000000000000000000000000000000000000000100001;
+    FILE *file1 = fopen("microprog.rom","wb");
+    assert(file1);
+    fwrite( teste, sizeof(teste[0]), 512, file1);
+    fclose(file1);
+
+    FILE *file2 = fopen("microprog.rom","rb");
+    assert(file2);
+    fread( teste, sizeof(teste[0]), 512, file2);
+    fclose(file2);
 
     for(int i = 0; i < 512; i++)
     {
         printf("%d: ", i+1);
         for(int j = 63; j >= 0; j--)
         {
-            printf("%lu", ((unsigned long)(teste[i] >> j) & 0b1));
+            printf("%lu", ((microcode)(teste[i] >> j) & 0b1));
         }
         printf("\n");
-    }
-
-    fclose(file);
+    };
 
     return 0;
 }
